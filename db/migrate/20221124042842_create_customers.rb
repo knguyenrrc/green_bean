a1846dbc2b77ccfbeddc21c53f41efcda57de5dc
Khanh Nguyen <knguyen77@academic.rrc.ca>
2022-11-23 22:29:45 -0600

    Created customer model

---
+++ db/migrate/20221124042842_create_customers.rb
@@ -0,0 +1,12 @@
+class CreateCustomers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :customers do |t|
+      t.string :customer_name
+      t.integer :phone
+      t.string :email
+      t.references :Order, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
