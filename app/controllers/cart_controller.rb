b26b0b236246dd56e0ee53bf81c1644ba6449f25
Khanh Nguyen <knguyen77@academic.rrc.ca>
2022-12-09 00:31:12 -0600

    Added functions to cart controller

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,2 +1,21 @@
 class CartController < ApplicationController
+    def create
+        id = params[:id].to_i
+
+        unless session[:shopping_cart].include?(id)
+            product = Product.find(id)
+            session[:shopping_cart] << id
+            flash[:notice] = "#{product.product_name} added to cart."
+        end
+
+        redirect_to root_path
+    end
+
+    def destroy
+        id = params[:id].to_i
+        session[:shopping_cart].delete(id)
+        product = Product.find(id)
+        redirect_to root_path
+        flash[:notice] = "#{product.product_name} removed from cart."
+    end
 end
