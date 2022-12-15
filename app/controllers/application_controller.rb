3b278758fdae06ae133d3599e4060c7a2824356b
Khanh Nguyen <knguyen77@academic.rrc.ca>
2022-12-15 06:55:16 -0600

    Revert "Merge pull request #5 from knguyenrrc/shopping_cart"
    This reverts commit 1ca1aeedf87db2321294de2fe8c4b507471e75d0, reversing
    changes made to 60484e2ea7a85ddc28ebf108d4503ca77f09b908.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,2 @@
 class ApplicationController < ActionController::Base
-    before_action :initialize_session
-    helper_method :cart
-
-    private
-
-    def initialize_session
-        session[:shopping_cart] ||= []
-    end
-
-    def cart
-        Product.find(session[:shopping_cart])
-    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,12 +8,10 @@ Rails.application.routes.draw do
       get "search"
     end
   end
-  resources :cart, only: [:create, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "products#index"
   get "/contact", to: "contact#index", as: "contact" # contact_path
   get "/about", to: "about#index", as: "about" # about_path
-
 end
